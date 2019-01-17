f24fe356a498292a51054b98180a03b94f6e7247
Dariusch Ochlast <dariusch.ochlast@avocadostore.de>
2019-01-17 09:26:36 +0100

    Redundant SafeVarargs annotation

--- src/main/java/de/pansoft/lucene/search/traversal/QueryTraverser.java
+++ src/main/java/de/pansoft/lucene/search/traversal/QueryTraverser.java
@@ -5,12 +5,11 @@ import org.apache.lucene.search.Query;
 public class QueryTraverser {
 
 	private final QueryHandler[] queryHandlers;
-	
-	@SafeVarargs
+
 	public QueryTraverser(final QueryHandler ...queryHandlers) {
 		this.queryHandlers = queryHandlers;
 	}
-	
+
 	public Query traverse(final Query query) {
 		for (QueryHandler queryHandler : queryHandlers) {
 			if (queryHandler.acceptQuery(query)) {
